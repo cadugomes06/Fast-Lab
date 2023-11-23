a9e3b31ca4708f824df436a425699fb6d2e961b1
Carlos Eduardo <cadu.010808@gmail.com>
2023-11-23 18:57:32 -0300

    component de loader identificando o navegador do usuário, erro no safari

--- src/components/LoadingCup.tsx
+++ src/components/LoadingCup.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 
 const LoadingCup = () => {
+    const [typeNavigator, setTypeNavigator] = useState('')
+
+        //Identificar o navegador
+        useEffect(() => {
+            if (navigator.userAgent.indexOf('Chrome') > -1 && navigator.userAgent.indexOf('Safari') > -1) {
+              setTypeNavigator('chrome')
+            } else if (navigator.userAgent.indexOf('Safari') > -1) {
+              setTypeNavigator('safari')
+            }
+        }, [])
+        console.log(typeNavigator)
 
     return (
-        <section className="w-full h-full max-h-max fixed top-0 z-50 bg-black/80 flex justify-center items-center sm:h-[100vh]">
+        <section className={` ${typeNavigator == 'safari' ? 'absolute' : 'fixed'} w-full h-full max-h-max  top-0 z-50 bg-black/80 flex justify-center items-center sm:h-[100vh]`}>
 
             <div className="cup"></div>
+
 
         </section>
     )
